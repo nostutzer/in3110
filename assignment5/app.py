dff2f1bccaae253611e39f3bb2761c07c5675a97
SagittariusA-Star <nilsole2009@live.no>
2022-11-18 13:58:04 +0100

    Added doctrings and comments, and did some cleanup to webapp code

--- assignment5/app.py
+++ assignment5/app.py
@@ -7,6 +7,7 @@ from fastapi import FastAPI, Query, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from starlette.staticfiles import StaticFiles
+import starlette
 from strompris import (
     ACTIVITIES,
     LOCATION_CODES,
@@ -21,31 +22,38 @@ app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 location_code_keys = tuple(LOCATION_CODES.keys())
 
-# `GET /` should render the `strompris.html` template
-# with inputs:
-# - request
-# - location_codes: location code dict
-# - today: current date
-
-
 @app.get("/", response_class=HTMLResponse)
 def render_strompris(
     request: Request,
-    locations_codes: List[str] = Query(default=location_code_keys),
     today: Optional[str] = Query(default=None),
 ):
-    locations = [LOCATION_CODES[location] for location in locations_codes]
+    """Function handles the "GET /" request from strømpris web app to
+       render the HTML template using Jinja.
+
+    Args:
+        request (Request): Request object from FastAPI.
+        today (Optional[str], optional): String of a date in ISO formate. Defaults to Query(default=None). 
+                                         If no date is provided the None will be changes to the date of today.
+
+    Returns:
+        _type_: Template response to strompris.html HTML template with parameters as a dict
+                for rendering the HTML page.
+                
+    """
+
+    # If no date is provided the current date is used.
+    # Else the provided date is used.
     if today is None:
         today = datetime.date.today()
     else:
         today = datetime.date.fromisoformat(today)
 
+    # Returning response to render HTML template
     return templates.TemplateResponse(
         "strompris.html",
         {
             "request": request,
             "location_codes": LOCATION_CODES,
-            "locations": locations,
             "today": today,
         },
     )
@@ -63,14 +71,30 @@ def render_strompris(
 
 @app.get("/plot_prices.json", response_class=HTMLResponse)
 def plot_prices_json(
-    request: Request,
     locations: List[str] = Query(default=location_code_keys),
     end: Optional[str] = Query(default=None),
     days: int = Query(default=8),
-):
-    print(locations)
-    print(days)
-    print(end)
+) -> str:
+    """Function which handles the "GET /plot_prices.json" request and
+       returns an Altair chart with enegry prices from 
+       https://www.hvakosterstrommen.no/ API.
+
+
+    Args:
+        locations (List[str], optional): List of strings containing location keys in Norway
+                                         for which to retrieve a chart of energy prices. 
+                                         Defaults to Query(default=location_code_keys).
+        end (Optional[str], optional): End date up to which to retrieve energy prices. 
+                                       Defaults to Query(default=None).
+        days (int, optional): How many days back in time from end for which to retrieve energy prices. 
+                              Defaults to Query(default=8).
+
+    Returns:
+        str: json string formatting of Altair chart containing energy prices.
+    """
+
+    # If no date is provided the current date is used.
+    # Else the provided date is used.
     if end is None:
         end = datetime.date.today()
     else:
@@ -111,7 +135,7 @@ def plot_prices_json(
 
 
 if __name__ == "__main__":
-    # use uvicorn to launch your application on port 5000
     import uvicorn
 
+    # Run web application on ip addresss 127.0.0.1 at port 5000
     uvicorn.run(app, host="127.0.0.1", port=5000)
